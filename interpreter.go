4cc429c32a3def74d32efd87b6c89358ddb23eee
pedroortega <pedro.ortega@gmail.com>
2025-09-24 14:06:45 +0100

    Tied up some loose ends in enum construction.

--- interpreter.go
+++ interpreter.go
@@ -286,6 +286,10 @@ type TypeValue struct {
 // TypeValIn builds a VTType and pins its resolution environment explicitly.
 // Use this when exporting user-defined types from specific scopes.
 func TypeValIn(expr S, env *Env) Value {
+	if msg := validateEnumsJSONOnly(expr); msg != "" {
+		// Use the runtime error channel; callers are wrapped with caret formatting.
+		panic(rtErr{msg: msg})
+	}
 	return Value{Tag: VTType, Data: &TypeValue{Ast: expr, Env: env}}
 }
 
